b49aa1bf070feccbb8d6a2262eb949b2930bf1cb
Amit <Amit@TinMin>
2023-12-27 14:29:36 -0500

    First Comitt

---
+++ src/GitPack/ExampleForGit.java
@@ -0,0 +1,37 @@
+package GitPack;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+ 
+public class ExampleForGit {
+ 
+    public static void main(String[] args) {
+        // declaration and instantiation of objects/variables
+        WebDriver driver = new FirefoxDriver();
+        String baseUrl = " https://demo.guru99.com/test/newtours/";
+        String expectedTitle = "Welcome: Mercury Tours";
+        String actualTitle = "";
+ 
+        // launch Firefox and direct it to the Base URL
+        driver.get(baseUrl);
+ 
+        // get the actual value of the title
+        actualTitle = driver.getTitle();
+ 
+        /*
+         * compare the actual title of the page with the expected one and print
+         * the result as "Passed" or "Failed"
+         */
+        if (actualTitle.contentEquals(expectedTitle)){
+            System.out.println("Test Passed!");
+        } else {
+            System.out.println("Test Failed");
+        }
+       
+        //close Firefox
+        driver.close();
+       
+        // exit the program explicitly
+        System.exit(0);
+    }
+ 
+}
